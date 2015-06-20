01a0762731c6a04e52ab526aea5ef20172c1a9b8
Manny Acquah <emmanuelacquah@Manny-Macbook-Pro.local>
2015-06-20 14:52:24 +0200

    Minor updates

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+printf "Updating repos...\n"
+
 BIN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $BIN_DIR/.. && git pull origin master
 cd $BIN_DIR/../../crater.js && git pull origin master
+
+printf "Repos updated!\n"
